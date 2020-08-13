d487e673e102d305a6d777b1bad035d9e56c1594
Eric <erjseals@gmail.com>
2020-08-13 18:28:19 -0500

    add gitignore

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <vector>
+#include <string>
+
+
+std::vector<std::string> letterCombinations(std::string digits);
+static std::vector<std::string> t9 = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
 int main() 
 {
-    std::cout << "Hello" << std::endl;
+    letterCombinations("23");
+}
+
+std::vector<std::string> letterCombinations(std::string digits) 
+{
+    std::vector<std::string> res;
+    return res;
 }
