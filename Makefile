f4784548197425fb30f07e9ac1935e561c7c38b3
Eric <erjseals@gmail.com>
2020-08-14 14:43:13 -0500

    delete nth node from end

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 # $@ matches the target; $< matches the first dependent
 prog: main.o
-	g++ -o $@ $<
+	g++ -o $@ -Wall $<
 
 main.o: main.cpp
-	g++ -c $<
+	g++ -c -Wall -g $<
 
 clean:
 	rm prog main.o
--- main.cpp
+++ main.cpp
@@ -2,20 +2,72 @@
 #include <vector>
 #include <string>
 
-
-std::vector<std::string> letterCombinations(std::string digits);
-
+// Data Structures and Constants Necessary
 std::vector<std::string> t9 = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
 
+struct ListNode 
+{
+    int val;
+    ListNode* next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// Function Declarations
+
+// letterCominations returns a vector<string> that contains all possible 
+// strings given a numerical combination. This is like t9 texting. 
+// Example would be input "45" and returns "gj", "gk", "gl", "hj", "hk", "hl", "ij", "ik", "il" 
+std::vector<std::string> letterCombinations(std::string digits);
+
+// given the head of a list, removes the nth node from the end
+ListNode* removeNthFromEnd(ListNode* head, int n);
+
+// delete the entire list
+ListNode* deleteList(ListNode* head);
+
+// Entry point
 int main() 
 {
-    std::vector<std::string> lC;
-    std::string input;
-    std::cin >> input;
-    lC = letterCombinations(input);
-    for(auto& a : lC) 
-        std::cout << a << ' ';
-    std::cout << std::endl;   
+    
+}
+
+// Functions
+
+ListNode* deleteList(ListNode* head) 
+{
+    while(head) 
+    {
+        ListNode* next = head->next;
+        delete head;
+        head = next;    
+    }
+    return head;
+}
+
+ListNode* removeNthFromEnd(ListNode* head, int n) 
+{
+    if(!head) return head;
+
+    ListNode* checkIfNth = head;
+    for(int i = 0 ; i < n ; i++) 
+    {
+        if(!checkIfNth) return checkIfNth;
+        checkIfNth = checkIfNth->next;
+    }
+
+    // If this is nullptr at this point in function,
+    // then it is the nth node to remove
+    if(!checkIfNth) 
+    {
+        ListNode* next = head->next;
+        delete head;
+        head = nullptr;
+        return next;
+    }
+    head->next = removeNthFromEnd(head->next, n);
+    return head;
 }
 
 std::vector<std::string> letterCombinations(std::string digits) 
@@ -23,7 +75,7 @@ std::vector<std::string> letterCombinations(std::string digits)
     // Initialize the result vector with an empty string for the base case swap
     std::vector<std::string> res = {""};
 
-    for(int i = 0 ; i < digits.size() ; i++) 
+    for(unsigned int i = 0 ; i < digits.size() ; i++) 
     {
         // Digits need to range between 0 and 9.
         // For example, the ascii value for 3 is 51 and 0 is 48. 51 - 48 is 3.  
@@ -37,8 +89,8 @@ std::vector<std::string> letterCombinations(std::string digits)
 
         std::vector<std::string> temp;
 
-        for(int k = 0 ; k < res.size() ; k++) 
-            for(int j = 0 ; j < str.size() ; j++) 
+        for(unsigned int k = 0 ; k < res.size() ; k++) 
+            for(unsigned int j = 0 ; j < str.size() ; j++) 
                 temp.push_back(res[k] + str[j]);
 
         res.swap(temp);
